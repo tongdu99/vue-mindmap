4124d2db1869cb484348fe7cda2f242867c41935
Tong Du <tongdu@gmail.com>
2018-11-06 11:30:27 -0800

    add demo website docs folder for github page hosting

---
+++ docs/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="app">
+		<mindmap
+			:nodes="nodes"
+			:connections="connections"
+			:editable="true"
+		/>
+	</div>
+</template>
+
+<script>
+import map from "./map";
+
+export default {
+  name: "app",
+  data() {
+    return { ...map };
+  }
+};
+</script>
